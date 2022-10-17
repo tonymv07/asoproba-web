8364e735194ce1d80459b30a9551441407abb34f
Tony Mora <tonyajmv07@gmail.com>
2022-10-17 01:06:29 -0600

    navbar funcional 40%

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,7 +15,7 @@ export default {
 </script>
 
 <template>
-    <nav class="navbar navbar-expand-lg">
+    <nav class="navbar navbar-expand-lg bg-navbar">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Navbar</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
